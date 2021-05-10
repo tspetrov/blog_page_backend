ee40498f3b67d887227dad5399fa0767d5d40a13
Tsvetomir <petrov.tsvetomir@vistajet.com>
2021-05-10 09:27:20 +0300

    Initial commit

---
+++ src/main/java/com/example/blog_page_backend/BlogPageBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_page_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogPageBackendApplication.class, args);
+    }
+
+}
